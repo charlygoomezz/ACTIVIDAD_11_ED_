3f1e4f5d4511a5438bd9955a327297adafb8ef24
pierosanttana <piero.santtana@gmail.com>
2024-02-27 12:01:40 +0100

    add cambios en equipo y mainEquipo

--- actividad_GIT_2_ED/src/actividad_GIT_2/Equipo.java
+++ actividad_GIT_2_ED/src/actividad_GIT_2/Equipo.java
@@ -1,5 +1,98 @@
 package actividad_GIT_2;
 
+import java.util.Arrays;
+
 public class Equipo {
+
+	String nombre;
+	String[] jugadores;
+
+	public Equipo() {
+		super();
+	}
+
+	public Equipo(String nombre, String[] jugadores) {
+		super();
+		this.nombre = nombre;
+		this.jugadores = jugadores;
+	}
+
+	// Los objetos tendrán la opción de mostrar todos sus datos
+	// por pantalla (mediante el método toString())
+
+	@Override
+	public String toString() {
+		return "Equipo [nombre=" + nombre + ", jugadores=" + Arrays.toString(jugadores) + "]";
+	}
+
+	// Los objetos tendrán la opción de mostrar todos sus jugadores
+	public void mostrarJugadores() {
+		for (String i : jugadores) {
+			System.out.println(i);
+		}
+	}
+
+	// Los objetos tendrán la opción de devolver
+	// si existe un jugador pasado por parámetro.
+	public boolean estaElJugador(String jugador) {
+
+		for (String i : jugadores) {
+			if (i == null) {
+				continue;
+			} else if (i.equalsIgnoreCase(jugador)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Los objetos tendrán la opción de devolver el numero de jugadores que tienen.
+	public int cantidadJugadores() {
+		int cantidad = jugadores.length;
+		for (String i : jugadores) {
+			if (i == null || i.isBlank()) {
+				cantidad--;
+			}
+		}
+		return cantidad;
+	}
+
+	// Los objetos tendrán la opción de devolver si el número de jugadores
+	// que tienen es apto para jugar.
+	// Un equipo es apto para jugar si tiene al menos 7 jugadores.
+	public boolean cantidadJugadoresEsApto() {
+		if (this.cantidadJugadores() >= 7) {
+			return true;
+		}
+
+		return false;
+	}
+
+	// Los objetos tendrán la opción de devolver si una lista de
+	// jugadores pasada por parámetro es exactamente igual a su lista de jugadores
+
+	public boolean esIgualListaJugadores(Equipo equipo) {
+		if (this.cantidadJugadores() == equipo.cantidadJugadores()) {
+			for (String j : equipo.jugadores) {
+				if (!estaElJugador(j)) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	// Los objetos tendrán la opción de devolver si un equipo pasado por parámetro
+	// es exactamente igual al mismo. Un equipo es exactamente igual si tiene el
+	// mismo nombre y la misma lista de jugadores con los mismos nombres.
+	public boolean esIgual(Equipo equipo) {
+		if(this.nombre.equalsIgnoreCase(equipo.nombre)) {
+			if (this.cantidadJugadores() == equipo.cantidadJugadores() && this.esIgualListaJugadores(equipo)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
--- actividad_GIT_2_ED/src/actividad_GIT_2/mainEquipo.java
+++ actividad_GIT_2_ED/src/actividad_GIT_2/mainEquipo.java
@@ -1,10 +1,86 @@
 package actividad_GIT_2;
 
+import java.util.Scanner;
+
 public class mainEquipo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
 
+		Equipo[] arrayEquipos = new Equipo[3];
+		System.out.println(arrayEquipos.length);
+		
+		arrayEquipos[0] = new Equipo();
+		arrayEquipos[1] = new Equipo();
+		arrayEquipos[2] = new Equipo();
+		
+		for (Equipo equipo : arrayEquipos) {
+			pedirDatosEquipo(equipo, scanner);
+			System.out.println();
+		}
+		
+		for (int i = 0; i < arrayEquipos.length; i++) {
+			
+			System.out.println("Jugadores:");
+			arrayEquipos[i].mostrarJugadores();
+			System.out.println();
+			
+			System.out.println("Ingrese el nombre de un jugador:");
+			String jugador = scanner.nextLine();
+			System.out.println("Esta " + jugador + " en el equipo?: " + arrayEquipos[i].estaElJugador(jugador));
+			
+			System.out.println("Cantidad de jugadores: " + arrayEquipos[i].cantidadJugadores());
+			
+			System.out.println("El equipo '" + arrayEquipos[i].nombre + "' es apto? " + arrayEquipos[i].cantidadJugadoresEsApto());
+			System.out.println();
+					
+			System.out.println("Elige un equipo con su numero para comparar:");
+			int y = 0;
+			
+			for (Equipo equipo : arrayEquipos) {
+				System.out.println("[" + (y++) +"] " + equipo.nombre);;
+			}
+			int equipo = scanner.nextInt();
+			
+			
+			System.out.println("La lista de jugadores es igual? " + arrayEquipos[i].esIgualListaJugadores(arrayEquipos[equipo]));
+
+			System.out.println("El equipo es igual? " + arrayEquipos[i].esIgual(arrayEquipos[equipo]));
+			System.out.println();
+			
+			scanner.nextLine();
+			
+		}
+		
 	}
+		
+		private static void pedirDatosEquipo(Equipo equipo, Scanner scanner) {
+
+			System.out.println("NUEVO EQUIPO");
+			System.out.println("-------------");
+
+			System.out.println("Ingresa un [Nombre de Equipo]:");
+			equipo.nombre = scanner.nextLine();
+
+			System.out.println("Ingresa la cantidad de [Jugadores posibles]:");
+			int cantidad = scanner.nextInt();
+			scanner.nextLine();
+
+			String[] jugadores = new String[cantidad];
+			equipo.jugadores = jugadores;
+
+			for (int i = 1; i <= jugadores.length; i++) {
+				System.out.println("Ingresa el nombre del [Jugador " + i + "]:");
+				String nombre = scanner.nextLine();
+				if (!nombre.isBlank()) {
+					equipo.jugadores[i - 1] = nombre;
+				} else {
+					System.err.println("-> Vuelve a intentarlo.");
+					System.out.println();
+					i--;
+				}
+			}
+		}
 
 }
